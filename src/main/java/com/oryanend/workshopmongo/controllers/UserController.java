ea8f84d8830b96913d21e3e3f14b437c94592710
oryanend <ryanyamim@gmail.com>
2025-10-08 10:54:56 -0300

    POST user

--- src/main/java/com/oryanend/workshopmongo/controllers/UserController.java
+++ src/main/java/com/oryanend/workshopmongo/controllers/UserController.java
@@ -6,11 +6,10 @@ import com.oryanend.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,5 +32,13 @@ public class UserController {
     public ResponseEntity<UserDTO> findById(@PathVariable String id){
         UserDTO obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @Transactional
+    @PostMapping
+    public ResponseEntity<UserDTO> insert(@RequestBody UserDTO obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
