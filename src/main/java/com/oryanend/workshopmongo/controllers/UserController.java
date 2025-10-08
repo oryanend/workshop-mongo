691cdfcd2ad1b82f5ef21de32836cfba476fa54d
oryanend <ryanyamim@gmail.com>
2025-10-08 10:35:10 -0300

    GET user by id

--- src/main/java/com/oryanend/workshopmongo/controllers/UserController.java
+++ src/main/java/com/oryanend/workshopmongo/controllers/UserController.java
@@ -1,15 +1,18 @@
 package com.oryanend.workshopmongo.controllers;
 
 import com.oryanend.workshopmongo.models.dto.UserDTO;
+import com.oryanend.workshopmongo.models.entities.User;
 import com.oryanend.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -25,4 +28,10 @@ public class UserController {
         return ResponseEntity.ok().body(list);
     }
 
+    @Transactional(readOnly = true)
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        UserDTO obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
 }
--- src/main/java/com/oryanend/workshopmongo/services/UserService.java
+++ src/main/java/com/oryanend/workshopmongo/services/UserService.java
@@ -3,10 +3,12 @@ package com.oryanend.workshopmongo.services;
 import com.oryanend.workshopmongo.models.dto.UserDTO;
 import com.oryanend.workshopmongo.models.entities.User;
 import com.oryanend.workshopmongo.repositories.UserRepository;
+import com.oryanend.workshopmongo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -19,4 +21,9 @@ public class UserService {
         return list.stream().map(UserDTO::new).toList();
     }
 
+    public UserDTO findById(String id){
+        Optional<User> result = repository.findById(id);
+        User entity = result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
+        return new UserDTO(entity);
+    }
 }
